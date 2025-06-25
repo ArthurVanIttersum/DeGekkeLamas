16707dd7bd7c21acaa5e74e26314f4ff888df722
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-06-25 10:25:26 +0200

    score now has a boost to it when full

--- ProjectNewHorizons/Assets/Scripts/ScoringSystem/ScoreManager.cs
+++ ProjectNewHorizons/Assets/Scripts/ScoringSystem/ScoreManager.cs
@@ -1,14 +1,25 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    float score;
+    [SerializeField] float score;
+    float trueScore;
     private float cachedScore;
     public Slider scoreSlider;
+    public TMP_Text scoreDisplay;
     public float maxScore = 1000;
     public float startScore = 500;
     public float decreaseSpeed = 1;
+    public float updateSpeed = 1;
+    [Header("Boost stuff")]
+    public float boostDuration = 3;
+    public float boostMultiplier = 2;
+    public Color boostColor;
+    private float currentMultiplier = 1;
+    private bool boostActive;
     [Tooltip("Multiplies speed when bar is full")] public float speedMultiplier = 1;
     [Range(-1,1)] public float hueShift = 1;
     [Header("Points given")]
@@ -32,6 +43,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (scoreSlider != null) scoreSlider.value = 0;
         score = 0;
+        trueScore = 0;
     }
 
 
@@ -45,24 +57,44 @@ public class ScoreManager : MonoBehaviour
 
     public void IncreaseScore(float ptsToAdd)
     {
-        score += ptsToAdd;
+        if (!boostActive) score += ptsToAdd * currentMultiplier;
+        trueScore += ptsToAdd * currentMultiplier;
         if (scoreSlider != null)
         {
             scoreSlider.maxValue = maxScore;
-            scoreSlider.value = score - cachedScore;
+            //scoreSlider.value = score - cachedScore;
         }
-        if (score - cachedScore >= maxScore) OnScoreFull();
+        if (score - cachedScore >= maxScore && !boostActive) OnScoreFull();
+        if (scoreDisplay != null) scoreDisplay.text = Mathf.RoundToInt(trueScore).ToString();
     }
 
     void OnScoreFull()
     {
-        cachedScore += maxScore * .5f;
-        scoreSlider.value = score - cachedScore;
+        //scoreSlider.value = score - cachedScore;
         decreaseSpeed *= speedMultiplier;
         Image fill = scoreSlider.fillRect.GetComponent<Image>();
         Color.RGBToHSV(fill.color, out float h, out float s, out float v);
         fill.color = Color.HSVToRGB((h + hueShift) % 1, s, v);
         print("Score bar full!");
+        StartCoroutine(ScoreBoost());
+    }
+
+    IEnumerator ScoreBoost()
+    {
+        scoreSlider.value = maxScore;
+        Image fill = scoreSlider.fillRect.GetComponent<Image>();
+        Color cachedColor = fill.color;
+        fill.color = boostColor;
+        boostActive = true;
+        currentMultiplier = boostMultiplier;
+
+        yield return new WaitForSeconds(boostDuration);
+
+        currentMultiplier = 1;
+        boostActive = false;
+        fill.color = cachedColor;
+        cachedScore += (score - cachedScore) * .5f;
+        yield return new();
     }
 
     void OnScoreZero()
@@ -72,9 +104,11 @@ public class ScoreManager : MonoBehaviour
 
     private void Update()
     {
-        score -= decreaseSpeed * Time.deltaTime; 
-        scoreSlider.value = score - cachedScore;
-
-        if (score - cachedScore < 0) OnScoreZero();
+        if (!boostActive && GridActivator.isPlayingMatch3)
+        {
+            score -= decreaseSpeed * Time.deltaTime;
+            if (score - cachedScore < 0) OnScoreZero();
+            scoreSlider.value += (score - cachedScore - scoreSlider.value) * Time.deltaTime * updateSpeed;
+        }
     }
 }
