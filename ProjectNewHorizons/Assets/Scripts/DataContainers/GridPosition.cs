70acda198bcc761923c621f6763db3062132ea39
arthur <ajcvanittersum@gmail.com>
2025-06-30 12:58:55 +0200

    Animations 2.0
    This means that blocks don't teleport or apear and disapear, but there is an animation that makes them move and grow or shrink.

--- ProjectNewHorizons/Assets/Scripts/DataContainers/GridPosition.cs
+++ ProjectNewHorizons/Assets/Scripts/DataContainers/GridPosition.cs
@@ -5,5 +5,6 @@ public class GridPosition : MonoBehaviour
     /// <summary>
     /// X value of this is 2nd index of the grid array, Y value of this is the 1st index
     /// </summary>
-    public Vector2 index; 
+    public Vector2 index;
+    public Vector3 destinationPosition;//to get the position of where it is moving to. used for switching aimation logic.
 }
