35aeaea652443fe8a19d9d3a5f406c409539f05d
arthur <ajcvanittersum@gmail.com>
2025-07-02 14:47:45 +0200

    Improved UI book
    added funcionality for adding text display

--- ProjectNewHorizons/Assets/Scripts/UIBook.cs
+++ ProjectNewHorizons/Assets/Scripts/UIBook.cs
@@ -3,8 +3,16 @@ using UnityEngine.UI;
 
 public class UIBook : MonoBehaviour
 {
-    public Sprite[] pages;
+    public struct PageData
+    {
+        public Sprite customerSprite;
+        public string name;
+        public string description;
+    }
+    public PageData[] pages;
     public Image theImageRenderer;
+    public Text theNameText;
+    public Text theDescriptionText;
     public int currentPage = 0;
     
     public void TurnPageRight()
@@ -26,6 +34,8 @@ public class UIBook : MonoBehaviour
 
     private void SetPage()
     {
-        theImageRenderer.sprite = pages[currentPage];
+        theImageRenderer.sprite = pages[currentPage].customerSprite;
+        theNameText.text = pages[currentPage].name;
+        theDescriptionText.text = pages[currentPage].description;
     }
 }
