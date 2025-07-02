8f4591edc90ca12352e596d385f1baa97da5dffa
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-07-02 10:20:36 +0200

    implemented new sprites

--- ProjectNewHorizons/Assets/Scripts/Match3Grid/MatchGridSystem.cs
+++ ProjectNewHorizons/Assets/Scripts/Match3Grid/MatchGridSystem.cs
@@ -116,9 +116,13 @@ public class MatchGridSystem : MonoBehaviour
         float size = ingredientLisText.fontSize;
         for (int i = 0; i < dish.dishType.recipeIngredientsList.Length; i++)
         {
+            int index = i;
+            if (dish.dishType.name.Length > 12) index++;
+            //ingredientLisText.bounds
+
             RectTransform spawned = Instantiate(recipeIcon, ingredientLisText.transform);
             float scale = spawned.root.lossyScale.x;
-            spawned.position -= (new Vector3(0, size * 3 * i, 0) + iconOffset) * scale;
+            spawned.position -= (new Vector3(0, size * 3 * index, 0) + iconOffset) * scale;
             spawned.sizeDelta = new(size, size);
             spawned.GetComponent<RawImage>().texture = ingredientTypes
                 [Ingredient.FindByName(ingredientTypes, dish.dishType.recipeIngredientsList[i].name)].texture;
